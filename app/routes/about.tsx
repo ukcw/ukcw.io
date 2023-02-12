5e6b308361d1fe533128b566e9fb914c8cca20e1
Ulysses Kee <ulysseskcw96@gmail.com>
2023-02-13 01:55:33 +0800

    More structure

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -38,34 +38,45 @@ export default function About() {
             </div>
           </nav>
         </div>
-        <div className="max-w-3xl mx-auto text-slate-900 dark:text-slate-200 divide-y">
-          <div className="pb-4">
+        <div className="max-w-3xl mx-auto text-slate-900 dark:text-slate-200">
+          <div className="pb-4 divide-y">
             <h2 className="text-4xl pb-4">
               About Me
             </h2>
-            <p className="">
+            <p className="pt-4">
               Hi, I'm Ulysses.
               I graduated from the University of Warwick in 2020 with a Bachelor of Science in Management with Finance (First-Class Honors), where I took a mix of courses in Business, Finance, Economics, and Computer science
               and recently graduated from the National University of Singapore (NUS) with a Master of Computing (Highest Distinction).
               I'm interested in distributed systems, logic, and programming languages amongst other areas of computer science. I'm an incoming Data Engineering intern at Binance.
             </p>
           </div>
-          <div className="pb-4">
+          <div className="pb-4 divide-y">
             <h2 className="text-4xl pt-4 pb-4">
               Work Experience
             </h2>
+            <div className="pt-4">
+              <div>
+                <h3 className="text-2xl">Binance</h3>
+              </div>
+              <div>
+                <h3 className="text-2xl">Singapore Blockchain Innovation Programme (NUS-SBIP)</h3>
+              </div>
+              <div>
+                <h3 className="text-2xl">Cloudflare</h3>
+              </div>
+            </div>
           </div>
-          <div className="pb-4">
+          <div className="pb-4 divide-y">
             <h2 className="text-4xl pt-4 pb-4">
               Projects
             </h2>
           </div>
-          <div className="pb-4">
+          <div className="pb-4 divide-y">
             <h2 className="text-4xl pt-4 pb-4">
               Education
             </h2>
           </div>
-          <div className="pb-4">
+          <div className="pb-4 divide-y">
             <h2 className="text-4xl pt-4 pb-4">
               Skills
             </h2>
