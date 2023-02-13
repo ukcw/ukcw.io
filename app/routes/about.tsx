c9a94d2fab8573b0dc7b44108a3d115e23a5bf4b
Ulysses Kee <ulysseskcw96@gmail.com>
2023-02-14 01:38:49 +0800

    Add more details

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -78,7 +78,7 @@ export default function About() {
                 <ul className="pt-2 list-disc list-inside">
                   <li>Developed FlareTools, a web-based application using React and Cloudflare Workers, to help internal teams and partners gain better visibility over product configurations, and copy configuration records and settings easily.</li>
                   <li>Developed a set of REST API services deployed on Cloudflare Workers which was used to interface with the Cloudflare v4 APIs.</li>
-                  <li>Presented my project in a global company-wide meeting and published a blog about it.</li>
+                  <li>Presented my project in a global company-wide meeting and published a <a className="underline-offset-auto" href="https://blog.cloudflare.com/internship-experience-software-development-intern/">blog</a> about it.</li>
                 </ul>
               </div>
               <div className="pt-4">
@@ -96,11 +96,44 @@ export default function About() {
             <h2 className="text-4xl pt-4 pb-4">
               Projects
             </h2>
+            <div className="pt-4">
+              <div>
+                <h3 className="text-2xl">P2P Distributed Game</h3>
+                {/* <p className="pt-2"><span className="italic">Data Engineering Intern</span> &middot; Feb 2023 - Mar 2023</p> */}
+                <ul className="pt-2 list-disc list-inside">
+                  <li>Designed and developed a peer-to-peer distributed maze game in Java using Java RMI and implemented mutual
+                    exclusion between threads via locks.</li>
+                </ul>
+              </div>
+              <div className="pt-4">
+                <h3 className="text-2xl">Definitional Interpreter</h3>
+                {/* <p className="pt-2"><span className="italic">Software Engineering Intern</span> &middot; Aug 2022 - Feb 2023</p> */}
+                <ul className="pt-2 list-disc list-inside">
+                  <li>Developed a definitional interpreter and type checker for Scilla (an intermediate-level language smart contract
+                    language developed for the Zilliqa blockchain) in JavaScript.</li>
+                </ul>
+              </div>
+            </div>
           </div>
           <div className="pb-4 divide-y">
             <h2 className="text-4xl pt-4 pb-4">
               Education
             </h2>
+            <div className="pt-4">
+              <div>
+                <h3 className="text-2xl">National University of Singapore (NUS)</h3>
+                <p className="pt-2"><span className="italic">Master of Computing (Highest Distinction)</span> &middot; Jan 2021 - Jan 2023</p>
+                <p className="pt-2 font-bold">Relevant Coursework</p>
+                <p className="pt-2">Data Structures and Algorithms, Computer Organisation, Operating Systems, Artificial Intelligence,
+                  Software Engineering, Fundamentals of Logic, Programming Languages Implementation, Distributed Systems, Big Data Systems for Data Science</p>
+              </div>
+              <div className="pt-4">
+                <h3 className="text-2xl">University of Warwick</h3>
+                <p className="pt-2"><span className="italic">Bachelor of Science in Management with Finance (First-Class Honours)</span> &middot; Aug 2022 - Feb 2023</p>
+                <p className="pt-2 font-bold">Relevant Coursework</p>
+                <p className="pt-2">Econometrics, Database Systems, Data Science, Investment Management, Statistics</p>
+              </div>
+            </div>
           </div>
           <div className="pb-4 divide-y">
             <h2 className="text-4xl pt-4 pb-4">
@@ -109,6 +142,6 @@ export default function About() {
           </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 }
