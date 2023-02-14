91f080bf911b036db87160474654c7bdc4688546
Ulysses Kee <ulysseskcw96@gmail.com>
2023-02-14 22:35:03 +0800

    Add more fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     extend: {
       fontFamily: {
         'jetbrains-mono': ["JetBrains Mono", "monospace"],
+        'open-sans': ["Rubik Moonrocks", "cursive"],
+        'palanquin': ["Palanquin", "sans-serif"],
+        'fira-sans': ["Fira Sans", "sans-serif"],
       }
     },
   },
