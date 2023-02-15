42128c9a664ace84646326d970efd99b68728f29
Ulysses Kee <ulysseskcw96@gmail.com>
2023-02-15 13:22:20 +0800

    Minor change

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,7 +1,7 @@
 export default function Notes() {
   return (
-    <div className="m-20 mx-auto">
-      <h1 className="pl-8">To be added.</h1>
+    <div className="">
+      <h1 className="">To be added.</h1>
     </div>
   )
 }
