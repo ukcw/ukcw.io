a21e32bf29fc47bddaad7134f42825c3fdcb6a73
Ulysses Kee <ulysseskcw96@gmail.com>
2023-02-15 13:21:56 +0800

    Add short text on homepage

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,6 +3,9 @@ import { NavBar } from "~/components/navbar";
 export default function Index() {
   return (
     <div>
+      <div className="mx-auto md:px-20 md:max-w-7xl">
+        <h1 className="text-4xl">Hello! Welcome to my home on the internet. This site is still work-in-progress and there's more to come!</h1>
+      </div>
     </div>
   );
 }
