230ce2156618bde02d1c27c1a13353fca28c83cd
Ulysses Kee <ulysseskcw96@gmail.com>
2023-02-21 23:37:14 +0800

    Update favicon and related assets for site

--- app/root.tsx
+++ app/root.tsx
@@ -17,6 +17,26 @@ export const links: LinksFunction = () => {
       rel: "stylesheet",
       href: styles,
     },
+    {
+      rel: "apple-touch-icon",
+      href: "/apple-touch-icon.png",
+      sizes: "180x180",
+    },
+    {
+      rel: "icon",
+      href: "/favicon-32x32.png",
+      sizes: "32x32",
+      type: "image/png",
+    },
+    {
+      rel: "icon",
+      href: "/favicon-16x16.png",
+      sizes: "16x16",
+    },
+    {
+      rel: "manifest",
+      href: "/site.webmanifest",
+    },
   ];
 };
 
