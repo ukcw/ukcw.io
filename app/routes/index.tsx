e77644361b10aab68b9373a1e57bd84e78dcef4a
Ulysses Kee <ulysseskcw96@gmail.com>
2023-02-23 23:25:04 +0800

    Make homepage full-screen by default

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,7 +3,7 @@ import { NavBar } from "~/components/navbar";
 export default function Index() {
   return (
     <div>
-      <div className="mx-auto md:px-20 md:max-w-7xl">
+      <div className="mx-auto px-5 min-h-screen lg:max-w-7xl">
         <h1 className="text-4xl">Hello! Welcome to my home on the internet. This site is still work-in-progress and there's more to come!</h1>
       </div>
     </div>
