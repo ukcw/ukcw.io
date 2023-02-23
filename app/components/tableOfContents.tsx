4a7ef597c3149bf4bd8c2ea4acbb1e96045cd03f
Ulysses Kee <ulysseskcw96@gmail.com>
2023-02-23 21:57:25 +0800

    Add intersection observer functionality

--- app/components/tableOfContents.tsx
+++ app/components/tableOfContents.tsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import useHeadingsData from "~/hooks/useHeadingsData";
+import useIntersectionObserver from "~/hooks/useIntersectionObserver";
 
-const Headings = ({ headings }: { headings: any[] }) => (
+const Headings = ({ headings, activeId }: { headings: any[], activeId: string }) => (
   <ul>
     {headings.map((heading) => (
-      <li key={heading.id} >
+      <li key={heading.id} /*className={heading.id === activeId ? "bg-pink-500" : ""}*/>
         <a href={`#${heading.id}`} className="font-semibold" onClick={(e) => {
           e.preventDefault();
           document.querySelector(`#${heading.id}`)!.scrollIntoView({
@@ -13,7 +15,7 @@ const Headings = ({ headings }: { headings: any[] }) => (
         {heading.items.length > 0 && (
           <ul>
             {heading.items.map((child: any) => (
-              <li key={child.id} className="pl-4 font-thin">
+              <li key={child.id} /*className={`pl-4 font-thin ${child.id === activeId ? "bg-pink-500" : ""}`}*/>
                 <a href={`#${child.id}`} onClick={(e) => {
                   e.preventDefault();
                   document.querySelector(`#${child.id}`)!.scrollIntoView({
@@ -31,12 +33,14 @@ const Headings = ({ headings }: { headings: any[] }) => (
 
 // https://www.emgoto.com/react-table-of-contents/
 const TableOfContents = () => {
+  const [activeId, setActiveId] = useState("");
   const { nestedHeadings } = useHeadingsData();
+  useIntersectionObserver(setActiveId);
 
   return (
     <div className="mx-auto max-w-3xl">
       <nav aria-label="Table of Contents" className="sticky top-10">
-        <Headings headings={nestedHeadings} />
+        <Headings headings={nestedHeadings} activeId={activeId} />
       </nav>
     </div>
   )
