43cdf4e3625bfa31bd33612466a37e71a3a98994
Ulysses Kee <ulysseskcw96@gmail.com>
2023-03-02 09:48:21 +0800

    Add styling for dark mode toggle

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -91,12 +91,15 @@ export function NavBar() {
             }
           })}
         </ul>
-        <button className="px-5 py-2 hidden lg:block" onClick={toggleTheme}>
+        <button
+          className="px-2 py-2 hidden lg:block border-gray-200 dark:border-gray-500  rounded-full border-2 hover:border-black hover:dark:border-white"
+          onClick={toggleTheme}
+        >
           <div className="">
             {themeValue === 'dark' ? (
-              <FaRegMoon className="h-4 w-4 text-white" />
+              <FaRegMoon className="h-5 w-5 text-white " />
             ) : (
-              <FaSun className="h-4 w-4 text-black" />
+              <FaSun className="h-5 w-5 text-black " />
             )}
           </div>
         </button>
