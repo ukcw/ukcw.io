8bd7bed5adda08cbd61e383a9ecaa6ea1b5f8552
Ulysses Kee <ulysseskcw96@gmail.com>
2023-11-05 22:04:58 +0800

    formatting

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,8 @@
 import { Github, Linkedin, Twitter } from "lucide-react"
 
-import { Icons } from "@/components/icons"
 import { siteConfig } from "@/config/site"
+import { Icons } from "@/components/icons"
+
 import { TextThemeToggle } from "./text-theme-toggle"
 
 export function Footer() {
@@ -11,10 +12,12 @@ export function Footer() {
         <div className="flex items-center justify-between">
           <div className="flex-1 text-sm">
             {/* <span>All rights reserved</span> */}
-            <span className="text-sm leading-tight tracking-tighter text-muted-foreground">© Ulysses Kee 2023</span>
+            <span className="text-sm leading-tight tracking-tighter text-muted-foreground">
+              © Ulysses Kee 2023
+            </span>
           </div>
           <div className="">
-          <TextThemeToggle />
+            <TextThemeToggle />
           </div>
           <div className="flex flex-1 items-center justify-end gap-4">
             <a href={siteConfig.links.github} target="_blank" rel="noreferrer">
@@ -23,7 +26,11 @@ export function Footer() {
             <a href={siteConfig.links.twitter} target="_blank" rel="noreferrer">
               <Icons.twitter className="h-4 w-4 fill-current text-muted-foreground" />
             </a>
-            <a href={siteConfig.links.linkedin} target="_blank" rel="noreferrer">
+            <a
+              href={siteConfig.links.linkedin}
+              target="_blank"
+              rel="noreferrer"
+            >
               <Linkedin className="h-4 w-4 fill-current text-muted-foreground" />
             </a>
           </div>
